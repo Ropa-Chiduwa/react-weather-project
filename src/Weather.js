5ebe9aab72cba88a06b902bc3927e6169c6819c1
Ropa-Chiduwa <90056931+Ropa-Chiduwa@users.noreply.github.com>
2022-01-24 22:45:09 +0900

    added form, search and city

---
+++ src/Weather.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city.."
+          className="form-control"
+        />
+        <input type="submit" value="Search" className="btn btn-primary" />
+      </form>
+      <h1>Harare</h1>
+      <ul>
+        <li>Monday 23:07</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+        .
+      </div>
+      Hello from Weather
+    </div>
+  );
+}
