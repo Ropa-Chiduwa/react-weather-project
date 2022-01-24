60dd70c4f81afeb20faaf721e6fd1f6ad1ecfa42
Ropa-Chiduwa <90056931+Ropa-Chiduwa@users.noreply.github.com>
2022-01-24 21:41:12 +0900

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -3,5 +3,29 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1> Weather</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Ropa-Chiduwa"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Belinda Chiduwa
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Ropa-Chiduwa/react-weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
