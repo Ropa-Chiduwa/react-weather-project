678bfc74234cb32e12f27ebb7c8c3bdb94484b25
Ropa-Chiduwa <90056931+Ropa-Chiduwa@users.noreply.github.com>
2022-01-25 09:16:24 +0900

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,12 +5,19 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <input
-          type="search"
-          placeholder="Enter a city.."
-          className="form-control"
-        />
-        <input type="submit" value="Search" className="btn btn-primary" />
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
       </form>
       <h1>Harare</h1>
       <ul>
@@ -23,7 +30,8 @@ export default function Weather() {
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly cloudy"
           />
-          6°C
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
